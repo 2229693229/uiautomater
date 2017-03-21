d929df0f323d3cb7d01cdc38e4246de857b5fcfe
2229693229 <2229693229@qq.com>
2017-03-21 09:34:32 +0800

    all

--- UiAutomatorDebug/bin/classes.dex.d
+++
@@ -1,4 +0,0 @@
-D:\GitHub\UiAutomatorHelper\UiAutomatorDebug\bin\classes.dex : \
-D:\GitHub\UiAutomatorHelper\UiAutomatorDebug\bin\classes\com\jikexueyuan\demo\CtsHelper.class \
-D:\GitHub\UiAutomatorHelper\UiAutomatorDebug\bin\classes\com\jikexueyuan\demo\Demo1.class \
-D:\GitHub\UiAutomatorHelper\UiAutomatorDebug\bin\classes\com\jikexueyuan\demo\UiAutomatorHelper.class \
